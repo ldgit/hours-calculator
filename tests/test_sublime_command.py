1d96dfa4ec0a93425c4ec82ba3c29f508f7b4993
Danko Lucic <dlucic33@gmail.com>
2015-12-10 21:09:38 +0100

    reverted previous commit

--- tests/test_sublime_command.py
+++ tests/test_sublime_command.py
@@ -83,6 +83,3 @@ class TestSublimeCommand(unittest.TestCase):
 
     def assertThirdInsertParameterIsCalculationResult(self, expected_calculation_result):
         self.assertEqual(expected_calculation_result, self.view.third_insert_parameter)
-
-    def test_fail(self):
-        self.fail('just a test')
